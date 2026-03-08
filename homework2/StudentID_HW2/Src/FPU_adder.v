b2bad2ace1d4217f10adff1f6735ef2d1b568de6
mylinh911 <linhtrinh091101@gmail.com>
2026-03-08 13:17:31 +0700

    hw2

--- homework2/StudentID_HW2/Src/FPU_adder.v
+++ homework2/StudentID_HW2/Src/FPU_adder.v
@@ -128,13 +128,13 @@ module FPU_adder(
 
 
 //put ur design here
-	always @(posedge clk or posedge rst or posedge enable) begin
+	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			output_c <= 32'b0;
 			output_c_ready <= 1'b1;
 		end
 		else begin
-			// if (enable) begin
+			if (enable) begin
 				if (!(NaN_a || NaN_b || Inf_a || Inf_b || Zero_a || Zero_b)) begin
 					// Normal case
 					output_c <= {sum_sign, sum_exp_norm, sum_frac_norm[22:0]};
@@ -183,11 +183,11 @@ module FPU_adder(
 					output_c_ready <= 1'b1;
 				end
 			end
-			// else begin
-			// 	output_c <= output_c;
-			// 	output_c_ready <= output_c_ready;
-			// end
-		// end
+			else begin
+				output_c <= output_c;
+				output_c_ready <= output_c_ready;
+			end
+		end
 	end
   
 endmodule        
